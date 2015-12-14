33424367f0ddb76c8147fa4ba05ce959ea27c28c
joranvar <bart.post@gmail.com>
2015-12-14 19:54:48 +0100

    Make number of splits inputable, not branch length

--- Main.hs
+++ Main.hs
@@ -20,12 +20,13 @@ branch start = branch' [start]
         branch' points size = widen points ++ branch' (widen points) (size - 1)
           where widen [Point leftx lefty, Point rightx righty] = [Point (leftx + 1) (lefty + 1), Point (rightx - 1) (righty + 1)]
 
-tree :: Point -> Int -> Tree
-tree start size =
+tree :: Point -> Int -> Int -> Tree
+tree _ _ 0 = []
+tree start size splits =
   let trunks = trunk start size
       branches = branch (last trunks) size
   in
-    trunks ++ branches ++ concat [tree st (size `div` 2) | st <- take 2 $ reverse branches]
+    trunks ++ branches ++ concat [tree st (size `div` 2) (splits - 1) | st <- take 2 $ reverse branches]
 
 formatTree :: Int -> Tree -> [String]
 formatTree width = map (\points -> map (\x -> if x `elem` map (\(Point x _) -> x) points then '1' else '_') [1..width])
@@ -35,5 +36,5 @@ formatTree width = map (\points -> map (\x -> if x `elem` map (\(Point x _) -> x
 main :: IO ()
 main = do
   sizeStr <- getLine
-  let size = read sizeStr
-  mapM_ putStrLn $ reverse $ formatTree 100 $ tree (Point 50 0) size
+  let splits = read sizeStr
+  mapM_ putStrLn $ reverse $ formatTree 100 $ tree (Point 50 0) 16 splits
